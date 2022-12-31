b66ffafb9b65a161f3125020baa3d10df3ff5628
Ryan Anderson <business.ryananderson@gmail.com>
2022-12-31 10:57:41 -0700

    final commit final

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,7 +2,7 @@ import React, { state, useState } from "react";
 import "./Switch.css";
 import cx from "classnames";
 import Song from "../assets/brownNoise.wav";
-// import { Howl } from "howler";
+
 
 
 
@@ -19,20 +19,19 @@ const Switch = ({rounded = false}) => {
         console.log("hit")
 
         if (isPlaying) {
-            // Pause the song if it is playing
+            
             audioObj.audio.pause();
           } else {
       
-            // Play the song if it is paused
+            
             audioObj.audio.play();
           }
       
-        //   Change the state of song
-        //   this.setState({ isPlaying: !isPlaying });
+      
         audioObj.isPlaying = !isPlaying
     };
     
-    // const [song, useSound] = useState(false);
+
 
     const slidercx = cx('slider' , {
         'rounded': rounded
